058bc469075839aa7b1dba64e8e1a7059a7144be
Juliano Oliveira <juliano@hwit.com.br>
2025-08-07 13:56:34 -0300

    LVGL 9.4 LCD C example code considering ST7789 LCD from peripheral and Raspberry pi pico W (bitdoglab 6.4) with LVGL. ST7789 driver implemented in PIO, GUI was generated by EEZ Studio software.

---
+++ firmware/c_cpp/examples/2_LCD_pio_LVGL_img_slider_button/st7789_pio_lcd_lvgl/ui/actions.h
@@ -0,0 +1,16 @@
+#ifndef EEZ_LVGL_UI_EVENTS_H
+#define EEZ_LVGL_UI_EVENTS_H
+
+#include "../lvgl/lvgl.h" // Include LVGL header for image handling
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_EVENTS_H*/
---
+++ firmware/c_cpp/examples/2_LCD_pio_LVGL_img_slider_button/st7789_pio_lcd_lvgl/ui/styles.h
@@ -0,0 +1,16 @@
+#ifndef EEZ_LVGL_UI_STYLES_H
+#define EEZ_LVGL_UI_STYLES_H
+
+#include "../lvgl/lvgl.h" // Include LVGL header for image handling
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_STYLES_H*/
